af583e3a57aee3fb8e4aa3bc64566f768ea4d1c0
rahman <rahmannurhayadi@gmail.com>
2025-06-23 10:02:08 +0700

    fix(userMod) : import jwt

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
 })
 export class UsersModule {}
