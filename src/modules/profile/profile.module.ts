434c488f7d21425c90f0bf5bacbbb0d536a52d9e
rahman <rahmannurhayadi@gmail.com>
2025-06-23 10:00:21 +0700

    fix(profileModule) : fix import export cloudinary serv

--- src/modules/profile/profile.module.ts
+++ src/modules/profile/profile.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
 import { CloudinaryModule } from 'src/libs/cloudinary/cloudinary.module';
+import { CloudinaryService } from 'src/libs/cloudinary/cloudinary.service';
 
 @Module({
   imports: [CloudinaryModule],
   controllers: [ProfileController],
-  providers: [ProfileService],
+  providers: [ProfileService, CloudinaryService],
+  exports: [CloudinaryService],
 })
 export class ProfileModule {}
