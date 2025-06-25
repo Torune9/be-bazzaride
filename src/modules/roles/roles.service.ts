ee953ce23cc80a4990eb33bbcfa7e479506725d1
rahman <rahmannurhayadi@gmail.com>
2025-06-25 10:46:53 +0700

    feat(roleServ) : add RUD role

--- src/modules/roles/roles.service.ts
+++ src/modules/roles/roles.service.ts
@@ -1,4 +1,9 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -33,19 +38,86 @@ export class RolesService {
     });
   }
 
-  findAll() {
-    return `This action returns all roles`;
+  async findAll() {
+    const roles = await this.prismaService.role.findMany();
+    if (roles.length == 0) {
+      throw new HttpException(
+        {
+          message: 'role masih kosong',
+          statusCode: 400,
+        },
+        400,
+      );
+    }
+
+    return {
+      data: roles,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
+  async findOne(id: number) {
+    const role = await this.prismaService.role.findFirst({
+      where: {
+        id: {
+          equals: id,
+        },
+      },
+    });
+
+    if (!role) {
+      throw new NotFoundException({
+        message: 'role tidak ditemukan',
+        statusCode: 404,
+      });
+    }
+    return {
+      data: role,
+    };
   }
 
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
+  async update(id: number, updateRoleDto: UpdateRoleDto) {
+    if (updateRoleDto.name == undefined) {
+      throw new HttpException(
+        {
+          message: 'nama belum terdefinsi',
+          statusCode: 400,
+        },
+        400,
+      );
+    }
+    const existing = await this.prismaService.role.findUnique({
+      where: { id: id },
+    });
+
+    if (!existing) {
+      throw new NotFoundException(`Role dengan id ${id} tidak ditemukan`);
+    }
+
+    const isSame =
+      existing.name.toLowerCase() === updateRoleDto.name.toLowerCase();
+
+    if (isSame) {
+      return {
+        message: 'tidak ada perubahan role',
+      };
+    }
+
+    const updated = await this.prismaService.role.update({
+      where: { id },
+      data: { name: updateRoleDto.name },
+    });
+
+    return { data: updated };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} role`;
+  async remove(id: number) {
+    const isDeleteRole = await this.prismaService.role.delete({
+      where: { id: id },
+    });
+    if (isDeleteRole) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
