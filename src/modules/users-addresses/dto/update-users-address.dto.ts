a7fe8eca2b79096a9c213dbe023a7a71a2a25756
rahman <rahmannurhayadi@gmail.com>
2025-07-01 10:04:52 +0700

    fix : userUpdate dto

--- src/modules/users-addresses/dto/update-users-address.dto.ts
+++ src/modules/users-addresses/dto/update-users-address.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType, PickType } from '@nestjs/mapped-types';
 import { CreateUsersAddressDto } from './create-users-address.dto';
 
-export class UpdateUsersAddressDto extends PartialType(CreateUsersAddressDto) {}
+export class UpdateUsersAddressDto extends PartialType(
+  PickType(CreateUsersAddressDto, ['city', 'district'] as const),
+) {}
