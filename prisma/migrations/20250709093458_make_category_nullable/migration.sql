12b6131ffbe3ac02240968346792dd030c9853aa
rahman <rahmannurhayadi@gmail.com>
2025-07-09 16:39:05 +0700

    feat : add category column

---
+++ prisma/migrations/20250709093458_make_category_nullable/migration.sql
@@ -0,0 +1,13 @@
+-- AlterTable
+ALTER TABLE "Event" ADD COLUMN     "categoryId" TEXT;
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
